f00a5cde3d30fcb27f5365bad90f3d14e0be0fa9
89trillion-shenhua <76935887+89trillion-shenhua@users.noreply.github.com>
2021-04-19 10:20:43 +0800

    Add files via upload

---
+++ Assets/Scripts/Infantry.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Infantry : MonoBehaviour
+{
+    private Animation animation;
+        
+    void Start(){
+        animation = GetComponent<Animation>();
+    }
+
+    void Update () {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            animation.wrapMode = WrapMode.Loop;
+            animation.Play("Attack");
+        }
+        else if(Input.GetKeyDown(KeyCode.R))
+        {
+            animation.wrapMode = WrapMode.Loop;
+            animation.Play("Run");
+        }
+        else if(Input.GetKeyDown(KeyCode.I))
+        {
+            animation.wrapMode = WrapMode.Loop;
+            animation.Play("Wait");
+        }
+    }
+}
